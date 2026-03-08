55dfbe0b75a213887ad5fc42565ecce7eeded2c4
Kaia Kolk <kaiakolk@gmail.com>
2026-03-08 15:14:34 +0200

    FE - pass the generic type to apiCall

--- frontend/src/app/dashboard/group/page.tsx
+++ frontend/src/app/dashboard/group/page.tsx
@@ -15,7 +15,7 @@ export default function GroupPage() {
 
   useEffect(() => {
     // Assume groupId is 1 for demo
-    apiCall('/children/group/1').then((data) => setChildren(data));
+    apiCall<Child[]>('/children/group/1').then((data) => setChildren(data));
   }, [apiCall]);
 
   return (
