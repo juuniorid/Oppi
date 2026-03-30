44a1afa3b7852fb685db0548c6958889af8be418
karlkuppas <karl.kuppas@gmail.com>
2026-03-30 17:21:38 +0300

    Install material UI

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
+
 export default function DashboardPage() {
   return (
     <div>
       <h1>Welcome to Dashboard</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 }
