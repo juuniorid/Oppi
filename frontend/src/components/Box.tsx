2ca342df4699d67f26d6ce80c70ca7547e0cf0be
karlkuppas <karl.kuppas@gmail.com>
2026-04-05 20:23:43 +0300

    box component improved

--- frontend/src/components/Box.tsx
+++ frontend/src/components/Box.tsx
@@ -1,14 +1,21 @@
 import { SectionTitle } from '@/components/SectionTitle';
+import clsx from 'clsx';
 
 type BoxProps = {
   title?: string;
   children: React.ReactNode;
+  className?: string;
 };
 
-export function Box({ title, children }: BoxProps) {
+export function Box({ title, children, className }: BoxProps) {
+  let header = null;
+
+  if (title) {
+    header = <SectionTitle className="mb-4">{title}</SectionTitle>;
+  }
   return (
-    <div className="rounded-2xl bg-[#faf8f5] border p-4">
-      <SectionTitle className="mb-4">{title}</SectionTitle>
+    <div className={clsx('rounded-2xl bg-[#faf8f5] border p-4', className)}>
+      {header}
       {children}
     </div>
   );
