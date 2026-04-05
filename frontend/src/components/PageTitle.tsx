679818e4a02a58559ce760842df1b41667c57546
karlkuppas <karl.kuppas@gmail.com>
2026-04-05 18:27:53 +0300

    pagetitle improved

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -1,6 +1,13 @@
-export function PageTitle({ children }: { children: React.ReactNode }) {
+type PageTitleProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function PageTitle({ children, className }: PageTitleProps) {
   return (
-    <h1 className="text-xl md:text-3xl font-bold text-gray-900 ml-2 mb-6">
+    <h1
+      className={`text-xl md:text-3xl font-bold text-gray-900 ml-2 mb-6 ${className ?? ''}`}
+    >
       {children}
     </h1>
   );
