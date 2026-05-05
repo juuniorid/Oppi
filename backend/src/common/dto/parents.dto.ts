1ce8c5f328e5b5a0b0c107a62b2bb9de066171f1
hermanpetrov <herman.ijgd@gmail.com>
2026-05-06 02:31:22 +0300

    fix: dto review and made into similar format as other dto

--- backend/src/common/dto/parents.dto.ts
+++ backend/src/common/dto/parents.dto.ts
@@ -1,8 +1,29 @@
+import { IsEmail, IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class ParentDto {
-    id: number;
-    email: string;
-    firstName: string | null;
-    lastName: string | null;
-    phone: string | null;
-    role: string;
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    id!: number;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email!: string;
+
+    @IsOptional()
+    @IsString()
+    firstName!: string | null;
+
+    @IsOptional()
+    @IsString()
+    lastName!: string | null;
+
+    @IsOptional()
+    @IsString()
+    phone!: string | null;
+
+    @IsString()
+    @IsNotEmpty()
+    role!: string;
 }
